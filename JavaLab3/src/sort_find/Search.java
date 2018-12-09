4b91f3cd60b3d3174150a7d6f441136e0aa34a40
Pankush <Pankush@ridz>
2018-12-08 20:28:37 -0800

    Lab3 program committed

---
+++ JavaLab3/src/sort_find/Search.java
@@ -0,0 +1,15 @@
+package sort_find;
+
+public class Search {
+	
+	
+	int present(String str,String arr[],int n)
+{
+	for(int i=0;i<n;i++)
+	if(str.compareTo(arr[i])==0)
+		return 1;
+return 0;
+}}
+	
+
+
